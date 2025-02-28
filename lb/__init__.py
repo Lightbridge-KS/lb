f6ad040be24a491790294f963730382fa3fc8c12
Lightbridge-KS <ki11ip0.s.a.s@gmail.com>
2025-02-28 22:26:59 +0700

    add `extract_from_codeblock()`

--- lb/__init__.py
+++ lb/__init__.py
@@ -1,8 +1,18 @@
 
-from .core.fs import (list_dir, read_text_dir, read_text_file, write_text_file)
+from .core.fs import (
+    list_dir, 
+    read_text_dir, 
+    read_text_file, 
+    write_text_file
+    )
 from .markdown.md_to_df_str import read_md_to_df_str
 from .docx import (
     read_docx,
     read_docx_dir,
     read_docx_dir_df
+)
+
+from .core.block import (
+    extract_from_tags,
+    extract_from_codeblock,
 )
